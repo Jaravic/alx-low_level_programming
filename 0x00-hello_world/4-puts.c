e3cd1f346c92e1f2da8e8f7faa8515422fe906fa
Jaravic <jarav22@gmail.com>
2023-02-16 07:46:42 -0800

    Task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Pram prints put function
+ * Return : 0 (End)
+ */
+int  main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+	retutm (0);
+}
