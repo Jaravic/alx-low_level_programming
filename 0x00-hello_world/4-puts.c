8b4efe3915cac091b58b61c2354a8e855876efda
Jaravic <jarav22@gmail.com>
2023-02-16 08:54:46 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Programming prints put function
- * Return: 0 (Success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-retutm(0);
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
 
