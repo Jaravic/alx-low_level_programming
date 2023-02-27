45f00ddb5aef392102bc9cadda5b99beb7d498d7
Jaravic <jarav22@gmail.com>
2023-02-27 07:08:22 -0800

    Task9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,11 +8,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	for (; src[i] != '\0'; i++)
+	for (i = 0; i < (n - 1); i++)
 	{
-		dest[i] = src[i];
+		printf("%d, ", a[i]);
 	}
-	return (dest);
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
